bd2ee422734af10f12d00101d87543344faac62c
Emiryungul <emryng2002@gmail.com>
2024-11-07 10:53:45 +0300

    created a sanctum login and register
    created a token based sanctum login and register endpoint for my flutter application we can also access the user info by the /user endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -22,3 +23,7 @@ Route::get('/products', [ProductController::class, 'index']);
 //cart endpoints
 Route::post('/cart', [CartController::class, 'store']); // Add product to cart
 Route::get('/cart', [CartController::class, 'index']);  // Get all cart items
+
+//Auth endpoints
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
