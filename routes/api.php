5d61dbf4a6404597cfad2002116c6ddf449920b6
Emiryungul <emryng2002@gmail.com>
2024-12-02 21:11:47 +0300

    fixed add address endpoint

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FavoriteController;
-use App\Http\Controllers\AddressesController;
+use App\Http\Controllers\AddressController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
